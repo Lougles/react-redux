234756e599b0e3f8dc51b7bcd371e6dee4809948
vovacelidze <louglescom@gmail.com>
2022-10-05 16:43:44 +0300

    added simple redux with bank example

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
+  
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+  console.log(cash);
+  
+  const addCash = (cash) => {
+    dispatch({type: "ADD_CASH", payload: cash})
+  }
+  const getCash = (cash) => {
+    dispatch({type: "GET_CASH", payload: cash})
+  }
+  
   return (
     <div className="App">
+      <div style={{fontSize: '3rem'}}>{cash}</div>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Button onClick={() => addCash(Number(prompt()))} variant="success">Increase deposit</Button>
+        <Button onClick={() => getCash(Number(prompt()))} variant="danger">Withdraw deposit</Button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {legacy_createStore} from "redux";
+import {Provider} from "react-redux";
+
+const action = {type: '', payload: '/'}
+const defaultState = {
+  cash: 0,
+}
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type){
+    case "ADD_CASH":
+      return {...state, cash: state.cash + action.payload}
+    case "GET_CASH":
+      return {...state, cash: state.cash - action.payload}
+    default:
+      return state
+  }
+}
+
+const store = legacy_createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
