99dbd9e66d516d698a421539f161d93cd7b88193
vovacelidze <louglescom@gmail.com>
2022-10-09 10:29:44 +0300

    commit and create new branch

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,13 @@ import thunk from "redux-thunk";
 import { countReducer } from "./countReducer";
 import { studentsReducer } from "./studentsReducer";
 
+
+
 const rootReducer = combineReducers({
   countReducer,
   studentsReducer
 })
+
+
 
 export const store = legacy_createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
